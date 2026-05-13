673698d8665d5ff28cb22f57bb70f74dc290421c
Chae-Hyun45 <kimch4577@gmail.com>
2026-05-13 15:39:31 +0900

    phone test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.off_record"
-        minSdk = 36
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
